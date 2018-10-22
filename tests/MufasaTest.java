16b8f94f94f4a307752cbcd20f5079bcf98302ea
Sebastian W <sebastian.weidinger@student.tugraz.at>
2018-10-22 22:25:19 +0300

    Assignment finished

--- tests/MufasaTest.java
+++ tests/MufasaTest.java
@@ -31,96 +31,106 @@ public class MufasaTest {
 	
 	@Test//verifying external dependency
 	public void testCeckBankAccountAttachment_Valid_Deduct_Certain_Amount() {
-		//Mock set expectations
+		//Arrange
 		Date expireDate = new Date(2019, 10, 22);
 		EasyMock.expect(bank.checkCode("Peter Pan", "Mastercard", "553645671234", 
 				expireDate, 123)).andReturn(true);
+		int accountIndex = 0; 
 		//Mock ready
 		EasyMock.replay(bank);
 		Account account = new Account("Peter", "Pan", "Peter Pan", "Mastercard", "553645671234",
 				expireDate, 10.0); 
 		mufasa.addAccount(account);
-		mufasa.checkBankAccountAttachment(0, 123);
+		//Act
+		mufasa.checkBankAccountAttachment(accountIndex, 123);
 		//Assert
-		assertEquals(9.99, mufasa.getAccount(0).getBalance(), 0.0);
+		assertEquals(9.99, mufasa.getAccount(accountIndex).getBalance(), 0.0);
 	}
 
 	@Test//verifying external dependency
 	public void testCeckBankAccountAttachment_Invalid_No_Deduction() {
-		//Mock set expectations
+		//Arrange
 		Date expireDate = new Date(2019, 10, 22);
 		EasyMock.expect(bank.checkCode("Hans Pan", "Mastercard", "553645671234", 
 				expireDate, 123)).andReturn(false);
+		int accountIndex = 0;
 		//Mock ready
 		EasyMock.replay(bank);
 		Account account = new Account("Hans", "Pan", "Hans Pan", "Mastercard", "553645671234",
 				expireDate, 10.0); 
 		mufasa.addAccount(account);
-		mufasa.checkBankAccountAttachment(0, 123);
+		//Act
+		mufasa.checkBankAccountAttachment(accountIndex, 123);
 		//Assert
-		assertEquals(10.0, mufasa.getAccount(0).getBalance(), 0.0);
+		assertEquals(10.0, mufasa.getAccount(accountIndex).getBalance(), 0.0);
 	}
 	
 	@Test//verifying external dependency
 	public void testRefundMoney_Valid_Refund_Certain_Amount() {
-		//Mock set expectations
+		//Arrange
 		Date expireDate = new Date(2019, 10, 22);
 		EasyMock.expect(bank.refundDeductedMoney("Sepp Hold", "Visa", "553645671234", 
 				expireDate)).andReturn(true);
+		int accountIndex = 0;
 		//Mock ready
 		EasyMock.replay(bank);
 		Account account = new Account("Sepp", "Hold", "Sepp Hold", "Visa", "553645671234",
 				expireDate, 15.99); 
 		mufasa.addAccount(account);
-		mufasa.refundMoney(0);
+		//Act
+		mufasa.refundMoney(accountIndex);
 		//Assert
-		assertEquals(16.0, mufasa.getAccount(0).getBalance(), 0.0);
+		assertEquals(16.0, mufasa.getAccount(accountIndex).getBalance(), 0.0);
 	}
 	
 	@Test//verifying external dependency
 	public void testRefundMoney_Invalid_No_Refund() {
-		//Mock set expectations
+		//Arrange
 		Date expireDate = new Date(2019, 10, 22);
 		EasyMock.expect(bank.refundDeductedMoney("Sepp Isi", "Visa", "553645671234", 
 				expireDate)).andReturn(false);
+		int accountIndex = 0;
 		//Mock ready
 		EasyMock.replay(bank);
 		Account account = new Account("Sepp", "Isi", "Sepp Isi", "Visa", "553645671234",
 				expireDate, 15.99); 
 		mufasa.addAccount(account);
-		mufasa.refundMoney(0);
+		//Act
+		mufasa.refundMoney(accountIndex);
 		//Assert
-		assertEquals(15.99, mufasa.getAccount(0).getBalance(), 0.0);
+		assertEquals(15.99, mufasa.getAccount(accountIndex).getBalance(), 0.0);
 	}
 	
 	@Test//verifying external dependency
 	public void testCreateAccount_Valid_Add_Account_To_System() {
-		//Mock set expectations
+		//Arrange
 		Date birthdate = new Date(1991, 9, 20);
 		EasyMock.expect(captchaServer.checkRealPerson("Herbert", "Moor", birthdate)).andReturn(true);
 		//Mock ready
 		EasyMock.replay(captchaServer);
 		Account expectedAccount = new Account("Herbert", "Moor", "herbie", "password123", 
 				birthdate, "0043567834", "herbie@gmx.at"); 
+		//Act
 		Account createdAccount = mufasa.createAccount("Herbert", "Moor", "herbie", "password123", 
 				birthdate, "0043567834", "herbie@gmx.at");
 		//Assert
-		//overwritten the equals method 
+		//overwritten the equals method of class Account
 		assertEquals(expectedAccount, createdAccount);
 	}
 	
 	@Test//verifying external dependency
 	public void testCreateAccount_Invalid_No_Account_Creation() {
-		//Mock set expectations
+		//Arrange
 		Date birthdate = new Date(1991, 9, 20);
 		EasyMock.expect(captchaServer.checkRealPerson("Simon", "Maier", birthdate)).andReturn(false);
 		//Mock ready
 		EasyMock.replay(captchaServer);
 		Account expectedAccount = null;
+		//Act
 		Account createdAccount = mufasa.createAccount("Simon", "Maier", "simoi", "password123", 
 				birthdate, "0043677887", "simoi@gmx.at");
 		//Assert
-		//overwritten the equals method 
+		//overwritten the equals method of class Account
 		assertEquals(expectedAccount, createdAccount);
 	}
 }
